44ab768c5299c48e7531a6cff6ac0dd11f52045b
AndrewZael <alfonsoguzman@live.cl>
2018-12-31 14:58:59 -0300

    Conexion firebase y desplegando informacion en la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from '@angular/platform-browser'
+import { ErrorHandler, NgModule } from '@angular/core'
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular'
+import { SplashScreen } from '@ionic-native/splash-screen'
+import { StatusBar } from '@ionic-native/status-bar'
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from './app.component'
+//Pages
+import { HomePage } from '../pages/home/home'
+import { SubirPage } from '../pages/subir/subir'
+
+//Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4ReSjk4jssMRlj43RuH03ok3uV0zv1Rw",
+  authDomain: "gag-cd39b.firebaseapp.com",
+  databaseURL: "https://gag-cd39b.firebaseio.com",
+  storageBucket: "gag-cd39b.appspot.com",
+  messagingSenderId: '459562654628'
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SubirPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SubirPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
+import { SubirPage } from '../subir/subir'
+
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: Observable<any[]>;
+  constructor(private modalCtlr:ModalController,
+            private afDB: AngularFireDatabase) {
+        this.items = afDB.list('post').valueChanges();
+        console.log(this.items)
+  }
 
-  constructor(public navCtrl: NavController) {
-
+  mostrarModal(){
+    let modal = this.modalCtlr.create(SubirPage)
+    modal.present()
+    console.log('OK')
   }
 
 }
